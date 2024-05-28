db003ded835e5d4d249f30e797e96f709d3fee5e
vedavakkada <77647914+vedavakkada@users.noreply.github.com>
2024-05-28 13:48:13 -0400

    Create 2610.py

---
+++ 2610.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        freq = {}
+        maxfreq = 1
+        maxfreqnum = nums[0]
+        for elem in nums:
+            
+            if elem not in freq:
+                freq[elem] = 1 
+                    
+            else:
+                freq[elem] += 1
+                count = freq[elem]
+                if count >=  maxfreq:
+                    maxfreq = count
+                    maxfreqnum = elem
+        
+        res = [[maxfreqnum] for _ in range(maxfreq)]
+        freq[maxfreqnum] = 0
+        
+        for num in freq:
+            for i in range(freq[num]):
+                res[i]+= [num]
+        
+        return res
+
